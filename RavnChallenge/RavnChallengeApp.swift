ccd850d17a27cfa4e996e358397655caac018343
Gustavo Campos <guseducampos@gmail.com>
2020-11-07 10:27:24 -0600

    Initial Commit

---
+++ RavnChallenge/RavnChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RavnChallengeApp.swift
+//  RavnChallenge
+//
+//  Created by Gustavo Campos on 7/11/20.
+//
+
+import SwiftUI
+
+@main
+struct RavnChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
